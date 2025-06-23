9659784c6a0381800a7133069bbc5b396f8458ca
Adarsh Kumar Singh <123314058+adarshkrsingh07@users.noreply.github.com>
2025-06-23 11:45:32 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import Counter from './Counter';
+
+function App() {
+ return (
+  <div id="container">
+   <h1>Welcome to the State Demo</h1>
+    <Counter />
+     </div>
+      );
+      }
+
+      export default App;
